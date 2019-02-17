1ecbab975f07baaf7c81888dda8105bd79544151
Jolson Da Costa <jdacos47@in.ibm.com>
2019-02-17 12:08:14 +0530

    First commit

---
+++ scan.php
@@ -0,0 +1,33 @@
+<?php
+//Directory path here
+$dir = "folder11/";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    $fileList = array();
+    $i=0;
+    while (($file = readdir($dh)) !== false){
+      if(!preg_grep('/^([^.])/', scandir($file))){ //Checks for hidden files. Files starting with . (dot)
+        $fileList[$i]["path"] =  $dir.$file;
+        if(filemtime($file)){ //filemtime — Gets file modification time
+          $fileList[$i]["time"] =  date ("F d Y H:i:s.", filemtime($file));
+        }
+        else{
+          $fileList[$i]["time"] = null;
+        }
+        $i++;
+      }
+    }
+    closedir($dh);
+  }
+
+    $jsonObj = json_encode($fileList);
+    echo $jsonObj;
+}
+else{
+    echo "Not a directory";
+}
+
+
+?>
