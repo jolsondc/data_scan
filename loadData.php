2bc8ec01284963cd81cc2a9dcfacc450240631e4
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-19 18:38:54 +0530

    loadData api fix

--- loadData.php
+++ loadData.php
@@ -1,8 +1,12 @@
 <?php
 if( $_POST["name"] ){
   echo $_POST['name'];
-}
 
+}
+echo gethostname(); // may output e.g,: sandie
+
+// Or, an option that also works before PHP 5.3
+echo php_uname('n'); // may output e.g,: sandie
 if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
   {		
