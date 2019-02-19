264377b3e97c2b6ed28ca965399bd32e1f67c3f4
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-19 17:24:39 +0530

    new api

--- loadData.php
+++ loadData.php
@@ -1,6 +1,6 @@
 <?php
 $row = 1;
-if (($handle = fopen("test.csv", "r")) !== FALSE) {
+if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         echo "<p> $num fields in line $row: <br /></p>\n";
