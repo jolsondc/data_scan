a6bc263006e6d984188257bc3813fb897d844617
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-19 18:25:34 +0530

    new api

--- loadData.php
+++ loadData.php
@@ -1,4 +1,7 @@
 <?php
+ $dir= json_decode(stripslashes($dir_name),true);
+echo $dir
+
 if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
   {		
