8163752fa443afe5675c0d32243c7eac905180b7
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-19 17:28:33 +0530

    new api

--- loadData.php
+++ loadData.php
@@ -1,14 +1,15 @@
 <?php
 $row = 1;
 if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    /*while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
+       // echo "<p> $num fields in line $row: <br /></p>\n";
         $row++;
         for ($c=0; $c < $num; $c++) {
             echo $data[$c] . "<br />\n";
         }
-    }
+    }*/
+    echo $handle
     fclose($handle);
 }
 ?>
