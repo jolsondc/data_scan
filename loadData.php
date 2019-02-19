e2d9556c05c39eeb6ae2a1ddf304e2b4c708083b
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-19 18:17:42 +0530

    new api

--- loadData.php
+++ loadData.php
@@ -1,26 +1,11 @@
 <?php
-$row = 1;
-$finalStr="";
 if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
-    /*while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-       // echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
-    }*/
-  // Convert each line into the local $data variable
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
   {		
     $finalStr =$finalStr."<br />\n". implode(',', $data) ;
-
   }
-
-
   // Close the file
     fclose($handle);
     echo trim($finalStr,"<br />\n");
-
 }
 ?>
