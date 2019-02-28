afd3c8b0643ec38b80edcc87e913be52d6735225
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-28 14:21:01 +0530

    fixes

--- loadData.php
+++ loadData.php
@@ -4,14 +4,15 @@ if( $_REQUEST["name"] && strcmp($_REQUEST["name"],gethostname()) ==0|| strcmp($d
 if (($handle = fopen("/home/pi/storage/data.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
   {		
-    $finalStr =$finalStr."<br />\n". implode(',', $data) ;
+    $finalStr =$finalStr."\n". implode(',', $data) ;
   }
   // Close the file
     fclose($handle);
-    echo trim($finalStr,"<br />\n");
+    echo trim($finalStr,"\n");
 }
 }else{
   $myObj->error = "Authentication failed";
   $myJSON = json_encode($myObj);
-  echo $myJSON;}
+  echo $myJSON;
+}
 ?>
